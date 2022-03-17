2d354b2dc3d3e01d2345e82a3df07ae322dcaefd
AustinStapleton <S-austin.stapleton@lwtech.edu>
2022-03-17 07:03:25 -0700

    save

--- app/src/main/java/edu/lwtech/distributingdata/MainActivity.java
+++ app/src/main/java/edu/lwtech/distributingdata/MainActivity.java
@@ -2,7 +2,11 @@ package edu.lwtech.distributingdata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        SecondActivity.class);
+                //intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/lwtech/distributingdata/MainModel.java
@@ -0,0 +1,58 @@
+package edu.lwtech.distributingdata;
+
+public class MainModel {
+
+    String description, name, rank, turl, type;
+    MainModel()
+    {
+
+    }
+
+    public MainModel(String description, String name, String rank, String turl, String type) {
+        this.description = description;
+        this.name = name;
+        this.rank = rank;
+        this.turl = turl;
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getTurl() {
+        return turl;
+    }
+
+    public void setTurl(String turl) {
+        this.turl = turl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
